3a9f24796e47f399d156c49e61cc1176ca488545
TommyWithATiger <halvard.hummel@hotmail.com>
2016-09-15 17:36:59 +0200

    Updated the views to do their job better

--- band_booking/views.py
+++ band_booking/views.py
@@ -1,24 +1,36 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout as logout_user
 
 
 def login_page(request):
+    '''
+    Displays the login page if the user is not logged in, else it redirects to the index page
+    '''
+    if request.user.is_authenticated:
+        return redirect('band_booking:index')
     return render(request, 'band_booking/login.html')
 
 
 def login_authenticate(request):
+    '''
+    Authenticates the user and login if the username and password are correct, else the user is redirected to the login
+    page with an error
+    '''
     username = request.POST.get('username', None)
     password = request.POST.get('password', None)
     user = authenticate(username=username, password=password)
 
     if user is not None:
         login(request, user)
-        pass
+        return redirect('band_booking:index')
     else:
-        return redirect('login')
+        return redirect('band_booking:login')
 
 
-def logout_logic(request):
-    logout(request)
-    return redirect('login', {})
-
+def logout(request):
+    '''
+    Logs the user out, redirects to the login page
+    '''
+    logout_user(request)
+    return redirect('band_booking:login')
