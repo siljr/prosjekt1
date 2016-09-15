10cda0d34605f15ceec439ab4069865a205922d9
TommyWithATiger <halvard.hummel@hotmail.com>
2016-09-15 13:30:53 +0200

    Added the app to the installed apps

--- IT1901Website/settings.py
+++ IT1901Website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'band_booking.apps.BandBookingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
