2761d0134324102a05bb14911c293683f1d444b5
TommyWithATiger <halvard.hummel@hotmail.com>
2016-09-15 17:36:19 +0200

    Fixed an issue with a malformed url

--- band_booking/urls.py
+++ band_booking/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'band_booking'
 urlpatterns = [
     url(r'^login$', views.login_page, name='login'),
-    url(r'^logout$', views.login_page, name='logout'),
+    url(r'^logout$', views.logout, name='logout'),
     url(r'^auth$', views.login_authenticate, name='login_auth')
 ]
