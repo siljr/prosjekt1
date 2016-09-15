20a3a91460728c42b2b4aed96118488122d8e54f
TommyWithATiger <halvard.hummel@hotmail.com>
2016-09-15 13:31:35 +0200

    Basic views for the login

--- band_booking/urls.py
+++ band_booking/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    url(r'^login$', views.login_page, name='login'),
+    url(r'^logout$', views.login_page, name='logout'),
+    url(r'^auth$', views.login_authenticate, name='login_auth')
+]
--- band_booking/views.py
+++ band_booking/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def login_page(request):
+    return render(request, 'band_booking/login.html')
+
+
+def login_authenticate(request):
+    username = request.POST.get('username', None)
+    password = request.POST.get('password', None)
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        pass
+    else:
+        return redirect('login')
+
+
+def logout_logic(request):
+    logout(request)
+    return redirect('login', {})
+
