54cd017a8b3822246e0c9f6dbbdc3e024aaf0949
Bredervik <oojsann@gmail.com>
2016-09-15 13:33:32 +0200

    Litt DB-testing

--- mypage/urls.py
+++ mypage/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import include ,url
 
+from . import views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('mypage.urls')),
+    url(r'^$', views.index, name ='index'),
+    url(r'^contact/', views.contact, name ='contact'),
+
 ]
