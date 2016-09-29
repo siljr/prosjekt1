c8462f1d331a53b65dc9d6b0f1349c28e7ceda75
Anders Brevik <oojsann@gmail.com>
2016-09-29 13:45:10 +0200

    La til et skjult field som gir en konserts økonomiske resultat

--- band_booking/models.py
+++ band_booking/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here .
+# Create your models here.
 
 class Person(models.Model):
     first_name = models.CharField(max_length=20)
