36f585d7273f6a8f35f3dbe2d951b5cc99d996e9
TommyWithATiger <halvard.hummel@hotmail.com>
2016-10-02 23:33:02 +0200

    Created the actual implementation of the view and changed the name of the variable in the url

--- bookingsjef/urls.py
+++ bookingsjef/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 app_name = "bookingsjef"
 urlpatterns = [
-    url(r'^economy/concert/(?P<id>([0-9]+))/$', views.economic_result_concert, name="economic_result")
+    url(r'^economy/concert/(?P<concert_id>([0-9]+))/$', views.economic_result_concert, name="economic_result")
 ]
