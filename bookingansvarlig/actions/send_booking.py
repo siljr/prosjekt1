40f208c0eb020fe126401b4a4da4cd920f8da00d
weroniiika <weronika.ada@gmail.com>
2016-10-03 19:36:16 +0200

    added test method and comments

--- bookingansvarlig/actions/send_booking.py
+++ bookingansvarlig/actions/send_booking.py
@@ -6,6 +6,12 @@ __author__ = 'Weronika'
 
 
 def send_booking(booking: Booking):
+    """
+    Sends email with booking with info provided in booking parameter.
+    Raises ValueError if booking is not approved.
+    Returns True if email was send successfully and False if it failed because of SMTPException
+    """
+
     # norwegian signs do not work, coding fixes required
     if booking.status == Booking.APPROVED:
         email = EmailMessage(subject=booking.title_name, body=booking.email_text, to=[booking.recipient_email],
--- bookingansvarlig/tests.py
+++ bookingansvarlig/tests.py
@@ -10,23 +10,42 @@ __author__ = 'Weronika'
 
 # @override_settings(EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend')
 class SendBookingTest(TestCase):
+    """
+    Test class that checks if send_booking method sends mail correctly for booking object with status APPROVED and it
+    raises ValueError if bookingn is not approved.
+
+    Tests use mailbox "simulator" and don't send mails "for real". To send mail "for real" using gmail smtp server
+    configured in settings comment out line 11 of this file (@override_settings.... , right above class declaration)
+    and add real email for recipient_email parameter (in creation of Booking object in setUp() method). Run tests.
+    Notice that some tests will fail then, but the email will be send "for real".
+
+    Notice that gmail smtp server has some limit as to numbers of emails send per day (from 25-99?), so let's not overuse it.
+    """
+
     def setUp(self):
-        user = User.objects.create(email='user_email@qqq.com')
+        user = User.objects.create(email='fake_mail@fake.com')
         self.booking = Booking.objects.create(sender=user, title_name="mail_subject_test",
-                                              recipient_email='recipient@qqq.com')
+                                              recipient_email='fake_recipient@fake.com')
 
     def test_send_booking_approved(self):
         self.booking.status = Booking.APPROVED
         result = send_booking(self.booking)
+
+        # Test that send_booking returns True
         self.assertEqual(result, True)
-        # Test that one message has been sent.
+
+        # Test that one message has been sent, with correct subject and body
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(str(mail.outbox[0].subject), self.booking.title_name)
         self.assertEqual(str(mail.outbox[0].body), self.booking.email_text)
+
         # Test that booking status has been updated
         new_status = Booking.objects.get(pk=self.booking.id).status
         self.assertEqual(new_status, Booking.SENT)
 
     def test_send_booking_not_approved(self):
         self.booking.status = Booking.NOT_APPROVED
-        result = send_booking(self.booking)
+        # Test that send_booking raises exception
+        with self.assertRaises(ValueError) as e:
+            send_booking(self.booking)
+        self.assertEqual(type(e.exception), ValueError)
