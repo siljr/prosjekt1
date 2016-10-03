10a8dd02b82447781b7b942bae829b50019e4b6e
TommyWithATiger <halvard.hummel@hotmail.com>
2016-10-03 19:24:35 +0200

    Added permissions for bookingsjef

--- bookingsjef/permissions.py
+++ bookingsjef/permissions.py
@@ -3,6 +3,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import pre_migrate
 from django.contrib.auth import models as auth_models
 from django.dispatch import receiver
+from band_booking.models import Concert
 
 
 @receiver(pre_migrate, sender=auth_models)
@@ -11,4 +12,11 @@ def add_user_permissions(**kwargs):
     Adds Bookingsjef to the group list
     """
     # Should add permissions for some content type.
-    auth_models.Group.objects.get_or_create(name='Bookingsjef')
+    group, created = auth_models.Group.objects.get_or_create(name='Bookingsjef')
+    content_type = ContentType.objects.get_for_model(Concert)
+    permission, created_permission = Permission.objects.get_or_create(codename='view_concert_economic_results', name='Can see economic results for concerts',
+                                     content_type=content_type)
+    group.permissions.add(permission)
+
+
+
--- bookingsjef/urls.py
+++ bookingsjef/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-
+from django.contrib.auth.decorators import permission_required
 from . import views
 
 
 app_name = "bookingsjef"
 urlpatterns = [
-    url(r'^economy/concert/(?P<concert_id>([0-9]+))/$', views.economic_result_concert, name="economic_result")
+    url(r'^economy/concert/(?P<concert_id>([0-9]+))/$',permission_required('band_booking.view_concert_economic_results', login_url='/login')(views.economic_result_concert), name="economic_result")
 ]
