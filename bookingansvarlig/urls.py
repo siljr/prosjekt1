d1b7c8cc448e2fc8d454fc5c8aa10b924df390d8
weroniiika <weronika.ada@gmail.com>
2016-10-04 17:27:35 +0200

    first working draft of bookings overview

--- bookingansvarlig/urls.py
+++ bookingansvarlig/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^scenes$',
         permission_required('band_booking.view_scenes', login_url='/login')(views.ScenesListView.as_view()),
         name='scenes'),
+    url(r'^bookings$', views.BookingListView.as_view(), name='bookings'),
     url(r'^scenes/(?P<scene>([A-Za-z 0-9]+))/$', views.concert_scene, name='concert_scene'),
     url(r'^booking/create_offer$', views.create_booking_offer, name='create_booking_offer'),
 ]
