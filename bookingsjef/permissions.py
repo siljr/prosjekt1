0d2389260500f1ed96774755a3f50a4425dfb5f2
TommyWithATiger <halvard.hummel@hotmail.com>
2016-10-06 23:09:09 +0200

    Added a new permission for the role bookingsjef, which indicates that the user can view all booking offers

--- bookingsjef/permissions.py
+++ bookingsjef/permissions.py
@@ -3,20 +3,29 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import pre_migrate
 from django.contrib.auth import models as auth_models
 from django.dispatch import receiver
-from band_booking.models import Concert
+from band_booking.models import Concert, Booking
 
 
 @receiver(pre_migrate, sender=auth_models)
 def add_user_permissions(**kwargs):
     """
-    Adds Bookingsjef to the group list
+    Adds Bookingsjef to the group list, and adds several permissions to the bookingsjef group
     """
-    # Should add permissions for some content type.
     group, created = auth_models.Group.objects.get_or_create(name='Bookingsjef')
-    content_type = ContentType.objects.get_for_model(Concert)
-    permission, created_permission = Permission.objects.get_or_create(codename='view_concert_economic_results', name='Can see economic results for concerts',
-                                     content_type=content_type)
-    group.permissions.add(permission)
+    permission_can_view_economic_results, created = Permission.objects.get_or_create(
+            codename='view_concert_economic_results',
+            name='Can see economic results for concerts',
+            content_type=ContentType.objects.get_for_model(Concert)
+    )
+
+    permission_can_view_all_booking_offers, created = Permission.objects.get_or_create(
+        codename='view_all_booking_offers',
+        name='Can view all booking offers',
+        content_type=ContentType.objects.get_for_model(Booking)
+    )
+
+    group.permissions.add(permission_can_view_economic_results)
+    group.permissions.add(permission_can_view_all_booking_offers)
 
 
 
