a2025df1d1516c61ef4d7dd9eeadc0542e9c609f
TommyWithATiger <halvard.hummel@hotmail.com>
2016-10-06 21:40:36 +0200

    Updated the UI for the booking offer creation page

---
+++ bookingansvarlig/static/bookingansvarlig/scripts/concert_overview.js
@@ -0,0 +1,14 @@
+function hideOnClick(elementId){
+    var element = document.getElementById(elementId);
+
+    if (element != null){
+        element.addEventListener("click", function(){
+            element.style.display = "none";
+        })
+    }
+}
+
+window.onload = function(){
+    hideOnClick("saved-message");
+    hideOnClick("offer-status-message");
+};
