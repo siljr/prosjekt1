a219c80bda7d32aebae34e557e99aaa1dec92163
Alfred Ronning <alfredronning@gmail.com>
2016-10-12 10:34:17 +0200

    begynte litt på urls

--- bookingsjef/urls.py
+++ bookingsjef/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 app_name = "bookingsjef"
 urlpatterns = [
-    url(r'^economy/concert/(?P<concert_id>([0-9]+))/$',permission_required('band_booking.view_concert_economic_results', login_url='/login')(views.economic_result_concert), name="economic_result")
+    url(r'^economy/concert/(?P<concert_id>([0-9]+))/$',permission_required('band_booking.view_concert_economic_results', login_url='/login')(views.economic_result_concert), name="economic_result"),
+    url(r'^economy/price_generator/$', permission_required('band_booking.view_concert_economic_results', login_url='/login')(views.price_generator), name="price_generator")
 ]
