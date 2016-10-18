0fd698cade6813da11dd0096848d79b046f73534
TommyWithATiger <halvard.hummel@hotmail.com>
2016-10-18 22:24:59 +0200

    Added collection of information for the calendar widget and changed some small things about the collection of information for the overview page

--- bookingsjef/actions/concert_overview_term.py
+++ bookingsjef/actions/concert_overview_term.py
@@ -1,5 +1,7 @@
 from django.utils import timezone
+from django.shortcuts import reverse
 from band_booking.models import Booking, Concert, Scene
+from calendar import monthrange
 
 
 def django_date(year, month, day):
@@ -8,15 +10,18 @@ def django_date(year, month, day):
 
 def get_current_term():
     current_date = timezone.now()
-
-    if current_date.month <= 6:
-        return django_date(current_date.year, 1, 1), django_date(current_date.year, 6, 31)
-    return django_date(current_date.year, 7, 1), django_date(current_date.year, 12, 31)
+    return get_term(current_date.month, current_date.year)
 
 
-def get_number_of_booking_offers_sent_this_term():
+def get_term(month, year):
+    if month <= 6:
+        return django_date(year, 1, 15), django_date(year, 5, 15)
+    return django_date(year, 8, 15), django_date(year, 12, 1)
+
+
+def get_number_of_booking_offers_sent_this_term(scene):
     start_term, end_term = get_current_term()
-    return len(Booking.objects.filter(date__lt=end_term, date__gt=start_term))
+    return len(Booking.objects.filter(date__lt=end_term, date__gt=start_term, scene__scene_name__icontains=scene))
 
 
 def get_number_of_booked_dates_this_term(scene):
@@ -30,11 +35,43 @@ def get_number_of_unbooked_dates_this_term(scene):
 
 
 def get_information_this_term():
+    term = get_current_term()
     return {
-        'booking_offers': get_number_of_booking_offers_sent_this_term(),
         'scenes': [{
             'name': scene[1].title(),
             'booked': get_number_of_booked_dates_this_term(scene[1]),
             'unbooked': get_number_of_unbooked_dates_this_term(scene[1]),
+            'offers': get_number_of_booking_offers_sent_this_term(scene[1]),
+            'calendar': reverse('bookingsjef:calendar', kwargs={'year': term[0].year, 'month': term[0].month, 'scene': scene[1]})
         } for scene in Scene.SCENE_CHOICES],
     }
+
+
+def build_information_month(year, month, scene):
+    start, days_in_month = monthrange(year, month)
+    term = get_term(month, year)
+    information = {
+        'scene': scene[0].upper() + scene[1:],
+        'empty_dates': range(start),
+        'empty_dates_end': range(7 - (start + days_in_month) % 7),
+        'dates': [{'date': date + 1} for date in range(days_in_month)],
+        'month': ["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"][month-1],
+    }
+
+    for date in information['dates']:
+        concerts_date = Concert.objects.filter(date=django_date(year, month, date['date']), scene__scene_name__icontains=scene)
+        if concerts_date:
+            date['booked'] = 'booked'
+            date['band'] = concerts_date[0].bands.all()[0].band_name
+            continue
+        booking_offers = Booking.objects.filter(date=django_date(year, month, date['date']), scene__scene_name__icontains=scene)
+        if booking_offers:
+            date['booked'] = 'offer-sent'
+            continue
+        date['booked'] = 'not-booked'
+    if term[0].month != month:
+        information["previous"] = reverse('bookingsjef:calendar', kwargs={'year': year, 'month': month - 1, 'scene': scene})
+    if term[1].month != month:
+        information["next"] = reverse('bookingsjef:calendar', kwargs={'year': year, 'month': month + 1, 'scene': scene})
+    return information
+
