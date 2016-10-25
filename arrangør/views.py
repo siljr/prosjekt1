7964af4141560124bcb98809547ff031bcfb263e
Halvard Hummel <halvarhu@stud.ntnu.no>
2016-10-25 10:48:29 +0200

    Added the view for the concert page

--- arrangør/views.py
+++ arrangør/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from band_booking.models import Concert
 
@@ -12,3 +12,15 @@ class ConcertsView(generic.ListView):
     def get_queryset(self):
         concerts = Concert.objects.filter(date__range=["2016-08-16", "2016-11-28"])
         return concerts
+
+
+def overview_concert(request, id):
+    try:
+        concert = Concert.objects.get(pk=id)
+    except Concert.DoesNotExist:
+        return redirect('arrangør:concerts')
+
+    if not request.user.is_superuser and request.user != concert.organizer:
+        return redirect('arrangør:concerts')
+
+    return render(request, 'arrangør/concert.html', {'concert': concert})
