7377795b54e45a2e08461e5b610802062bdb9394
Halvard Hummel <halvard.hummel@hotmail.com>
2016-10-26 14:26:18 +0200

    Added view for techincal requirements to for manager of band

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from band_booking.models import Technical_needs, Band
 # Create your views here.
+
+
+def technical_requirements(request):
+    try:
+        band = Band.objects.get(manager=request.user)
+    except Band.DoesNotExist:
+        # TODO: Add error message
+        return redirect('band_booking:index')
+    equipment = Technical_needs.objects.filter(band=band)
+    equipment_information = []
+    for current_equipment in equipment:
+        equipment_information.append({
+            'pk': current_equipment.pk,
+            'name': current_equipment.Equipment_name,
+            'number': current_equipment.amount,
+        })
+    return render(request, 'manager/equipment_collection.html', {'equipmentCollection': equipment_information})
+
+
+def update_technical_requirements(request):
+    equipment_number = request.POST.getlist('equipment_number')
+    equipment_pk = request.POST.getlist('pk')
+    equipment_name = request.POST.getlist('equipment_name')
+    delete = request.POST.getlist('delete')
+    print(equipment_number)
+    for index in range(len(equipment_number)):
+        try:
+            equipment = Technical_needs.objects.get(pk=equipment_pk[index])
+            if delete[index] == '1':
+                equipment.delete()
+                continue
+            if equipment.band.manager != request.user or int(equipment_number[index]) < 1 or len(equipment_name[index]) < 1:
+                continue
+            equipment.Equipment_name = equipment_name[index]
+            equipment.amount = int(equipment_number[index])
+            equipment.save()
+        except Technical_needs.DoesNotExist:
+            continue
+    return redirect('manager:technical_requirements')
