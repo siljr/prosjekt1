0b88148c55003a6bec55e134c19b7256c4a44d3e
Halvard Hummel <halvard.hummel@hotmail.com>
2016-10-26 14:26:53 +0200

    Fixed some issues with the HTML and JS

--- manager/static/manager/scripts/delete_on_click.js
+++ manager/static/manager/scripts/delete_on_click.js
@@ -1,20 +1,21 @@
 window.onload = function() {
 	var elements = document.getElementsByClassName("equipment-add-delete-button");
 	for(index = 0; index < elements.length; index++){
-		var element = elements.get(index);
-		element.addEventListener("onclick", function()){
-			var deleteInput = parent.childNodes.get(2);
-			if (deleteInput.value == "0") {
-				deleteInput.value = "1";
-				this.innerHTML = "Omgjør sletting";
-				parent.childNodes.get(0).setAttribute("disabled", "true");
-				parent.childNodes.get(1).setAttribute("disabled", "true");
+		var element = elements[index];
+		element.addEventListener("click", function(){
+            var childs = this.parentNode.childNodes;
+            console.log(childs)
+            if (childs[3].value == 0) {
+				childs[3].value = "1";
+				this.innerHTML = "OmgjÃ¸r sletting";
+				childs[1].setAttribute("readonly", "");
+                childs[2].setAttribute("readonly", "");
 			} else {
-				deleteInput.value = "0";
+				childs[3].value = "0";
 				this.innerHTML = "Slett utstyr";
-				parent.childNodes.get(0).removeAttribute("disabled");
-				parent.childNodes.get(1).removeAttribute("disabled");
+                childs[1].removeAttribute("readonly");
+                childs[2].removeAttribute("readonly");
 			}
-		}
+		});
 	}
 };
