55e44053ede8a2ce06c95ca88e61fbafd6368bb4
Halvard Hummel <halvard.hummel@hotmail.com>
2016-11-01 15:50:07 +0100

    Updated the form to select the band based on the manger

--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, reverse, redirect
+from band_booking.models import Band
 
 from band_booking.forms import ChangeTechnicalneedsForm
 
@@ -9,9 +10,15 @@ def changeTechnicalneed(request):
     if request.method == 'POST':
         form = ChangeTechnicalneedsForm(request.POST)
         if form.is_valid():
-            form.save()
+            technical_need = form.save(commit=False)
+            try:
+                band = Band.objects.filter(manager=request.user)[:1].get()
+                technical_need.band = band
+            except Band.DoesNotExist:
+                return redirect('/technical_needs/')
             # hvis dette fungerer går jeg tilbake til listen over behov.. Kanskje
             # return HttpResponseRedirect(reverse('changeNeeds'))
+            technical_need.save()
             return redirect('/technical_needs/')
 
     else:
