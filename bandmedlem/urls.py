53758b717df220fb5d3489ce12477ef15d84d4ae
Halvard Hummel <halvard.hummel@hotmail.com>
2016-11-01 17:12:57 +0100

    Added the URL and view for the page

--- bandmedlem/urls.py
+++ bandmedlem/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 app_name = "bandmedlem"
 urlpatterns = [
+    url(r'^band/calendar/(?P<year>([0-9]{4}))/(?P<month>(([1][0-2]|[1-9]{1})))/$', views.concerts_band, name='calendar')
 ]
