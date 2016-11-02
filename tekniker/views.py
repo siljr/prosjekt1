2a161c76226cff661cf0b4ceb0f433c905365469
Halvard Hummel <halvard.hummel@hotmail.com>
2016-11-02 12:42:48 +0100

    Filter out all concerts in the past, display an error message when the person is not scheduled to work at any concerts

--- tekniker/views.py
+++ tekniker/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from band_booking.models import Concert
+from datetime import datetime
 from django.contrib.auth.models import User
 # Create your views here.
 
 def tekniker_concerts(request):
-    print(type(request))
-    concerts = Concert.objects.filter(personnel=request.user)
-    context = {
-        'concerts': concerts
-    }
+    concerts = Concert.objects.filter(personnel=request.user, date__gte=datetime.now())
+    context = {}
+    if concerts:
+        context['concerts'] = concerts
     return render(request, 'tekniker/myconcerts.html', context)
