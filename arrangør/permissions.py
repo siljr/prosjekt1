317b6d5d4468473220f3dc7e2f8e16767cf730a1
Halvard Hummel <halvarhu@stud.ntnu.no>
2016-11-06 15:29:13 +0100

    Moved all apps to use the new permission manager

--- arrangør/permissions.py
+++ arrangør/permissions.py
@@ -1,9 +1,7 @@
-from django.contrib.auth.models import Permission
-from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import models as auth_models
 from django.db.models.signals import pre_migrate
 from django.dispatch import receiver
-from band_booking.models import Concert
+from permission_creator import add_permissions_group
 
 
 @receiver(pre_migrate, sender=auth_models)
@@ -11,12 +9,7 @@ def add_user_permissions(**kwargs):
     """
     Adds Arrangør to the group list
     """
-    group, created = auth_models.Group.objects.get_or_create(name='Arrangør')
 
-    permission_can_view_concert_information, created = Permission.objects.get_or_create(
-            codename='view_concert_information',
-            name='Can view concert information',
-            content_type=ContentType.objects.get_for_model(Concert)
-    )
-
-    group.permissions.add(permission_can_view_concert_information)
+    add_permissions_group("Arrangør", [
+        ("view_concert_information", "Can view concert information"),
+    ])
