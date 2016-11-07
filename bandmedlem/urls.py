ddc6329c95844f2ed21708ebe5ca934df0119c69
Adrian <Adri.g.joseph@gmail.com>
2016-11-07 11:20:15 +0100

    Small fixes

--- bandmedlem/urls.py
+++ bandmedlem/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 app_name = "bandmedlem"
 urlpatterns = [
+    url(r'^view_bandmedlem$', views.BookingListView.as_view(), name='bookings')
 ]
--- bandmedlem/views.py
+++ bandmedlem/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from band_booking.models import Booking
 
-# Create your views here.
+
+class BandListView(generic.ListView):
+    template_name = 'bandmedlem/view_for_offers.html'
+    context_object_name = 'bookings'
+
+    def get_queryset(self):
+        return [booking for booking in Booking.objects.all() if booking.user_allowed_to_view(self.request.user)]
+
